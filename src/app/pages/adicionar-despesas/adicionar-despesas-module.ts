4a1809d522660f22857cb22ae95eead2a87ed3a9
Jesiel Faria <fariajesiel@gmail.com>
2025-07-06 21:03:20 -0300

    Configuração base do adicionar receita e despesa

--- src/app/pages/adicionar-despesas/adicionar-despesas-module.ts
+++ src/app/pages/adicionar-despesas/adicionar-despesas-module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdicionarDespesas } from './adicionar-despesas';
+import { SidebarMenuModule } from '../../components/sidebar-menu/sidebar-menu-module';
 
 @NgModule({
   declarations: [AdicionarDespesas],
-  imports: [CommonModule],
+  imports: [CommonModule, SidebarMenuModule],
   exports: [AdicionarDespesas],
 })
 export class AdicionarDespesasModule {}
--- src/app/pages/adicionar-receitas/adicionar-receitas-module.ts
+++ src/app/pages/adicionar-receitas/adicionar-receitas-module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdicionarReceitas } from './adicionar-receitas';
+import { SidebarMenuModule } from '../../components/sidebar-menu/sidebar-menu-module';
 
 @NgModule({
   declarations: [AdicionarReceitas],
-  imports: [CommonModule],
+  imports: [CommonModule, SidebarMenuModule],
   exports: [AdicionarReceitas],
 })
 export class AdicionarReceitasModule {}
