4a6d5a6a1dd3ecefe1d208c939dbb12988e12aa0
Jesiel Faria <fariajesiel@gmail.com>
2025-10-23 10:02:18 -0300

    Implementando tela base da configuração

--- src/app/pages/configuracoes/configuracoes-module.ts
+++ src/app/pages/configuracoes/configuracoes-module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Configuracoes } from './configuracoes';
 import { SidebarMenuModule } from '../../components/sidebar-menu/sidebar-menu-module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [Configuracoes],
-  imports: [CommonModule, SidebarMenuModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SidebarMenuModule,
+    InputTextModule,
+    ButtonModule,
+  ],
   exports: [Configuracoes],
 })
 export class ConfiguracoesModule {}
--- src/app/pages/configuracoes/configuracoes.ts
+++ src/app/pages/configuracoes/configuracoes.ts
@@ -1,11 +1,72 @@
-import { Component } from '@angular/core';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { environmentDev } from '../../utils/environment';
+import { catchError, take, throwError } from 'rxjs';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-configuracoes',
   standalone: false,
   templateUrl: './configuracoes.html',
-  styleUrl: './configuracoes.scss'
+  styleUrl: './configuracoes.scss',
 })
-export class Configuracoes {
+export class Configuracoes implements OnInit {
+  userForm: FormGroup;
+  currentUser: any;
 
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {
+    this.userForm = this.fb.group({
+      nameUser: ['', [Validators.required]],
+      emailUser: ['', [Validators.required]],
+      passwordUser: ['', Validators.required],
+      newPasswordUser: ['', Validators.required],
+      confirmPasswordUser: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadUserData();
+  }
+
+  onUpdate() {
+    if (this.userForm.valid) {
+      // this.updateUser();
+      console.log('Fomulário válido');
+    } else {
+      console.error(
+        'O formulário não é válido ou os parâmetros de rota estão ausentes.'
+      );
+    }
+  }
+
+  private loadUserData() {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      this.router.navigate(['/login']);
+      return;
+    }
+
+    const headers = new HttpHeaders({ Authorization: `Bearer ${token}` });
+
+    this.currentUser = this.authService.getLoggedInUser();
+
+    if (this.currentUser) {
+      console.log('Configuração Usuário logado: ', this.currentUser);
+      this.userForm.get('nameUser')?.setValue(this.currentUser.name);
+      this.userForm.get('emailUser')?.setValue(this.currentUser.email);
+    } else {
+      console.log('Nenhum usuário logado ou token inválido.');
+    }
+  }
 }
